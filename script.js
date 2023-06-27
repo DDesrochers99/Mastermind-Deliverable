3976665c368610582660d618df6439f2a963e8ae
ddesrochers99 <desrochersdale@gmail.com>
2023-06-27 11:17:32 -0700

    Adds render function display of hidden code

--- script.js
+++ script.js
@@ -3,23 +3,50 @@ const colors = ["red", "blue", "green", "yellow", "pink", "gray"];
 const codeLength = 4;
 
 /*----- state variables -----*/
-const randomCode = createRandomCode();
-console.log("Random Code", randomCode);
+let randomCode;
+// randomCode = ["red", "blue", "green", "yellow"];
 /*----- cached elements  -----*/
 
 /*----- event listeners -----*/
 
 /*----- functions -----*/
+
+randomCode = createRandomCode();
+console.log("Random Code", randomCode);
+
+render();
+
 function createRandomCode() {
   const code = [];
   while (code.length < codeLength) {
-    for (let i = 0; i < codeLength; i++) {
-      const index = Math.floor(Math.random() * colors.length);
-      const color = colors[index];
-      if (!code.includes(color)) {
-        code.push(colors[index]);
-      }
+    const index = Math.floor(Math.random() * colors.length);
+    const color = colors[index];
+    if (!code.includes(color)) {
+      code.push(color);
     }
   }
   return code;
+}
+function getHiddenCode(hiddenCode) {
+  const code = [];
+  for (let i = 0; i < codeLength; i++) {
+    code.push(hiddenCode[i].style.backgroundColor);
+  }
+  return code;
+}
+
+function render() {
+  const hideCode = document.querySelectorAll(`.hiddenCode .hide`);
+  for (let i = 0; i < codeLength; i++) {
+    hideCode[i].style.backgroundColor = randomCode[i];
+  }
+  const playerGuess = document.querySelectorAll(`.playerGuess`);
+  for (let i = 0; i < playerGuess.length; i++) {
+    const guessBox = playerGuess[i].querySelectorAll(`.guess`);
+    const resultBox = playerGuess[i].querySelectorAll(`.result`);
+    for (let j = 0; j < codeLength; j++) {
+      guessBox[j].style.backgroundColor = `eggshell`;
+      resultBox[j].style.backgroundColor = `black`;
+    }
+  }
 }
